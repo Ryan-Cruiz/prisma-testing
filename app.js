bc072c64856c5a98d2ae93791e80fe9155e8aeb3
ryan-cruiz <ryancruiz97@gmail.com>
2025-03-02 12:42:05 +0800

    main refactor

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from "body-parser";
-import UserRoute from './routes/UserRoute.js';
-const app = express()
-const port = 3000
-app.use(express.json());
-app.use(bodyParser.urlencoded());
-app.use(cors());
-app.use(UserRoute);
 
-app.listen(port, () => {
-    console.log(`listening on port http://localhost:${port}`)
+// ESM
+import helmet from '@fastify/helmet';
+import Fastify from 'fastify';
+import rateLimit from '@fastify/rate-limit';
+import userRoute from './routes/UserRoute.js';
+import fastifyCors from '@fastify/cors';
+const fastify = Fastify({
+    logger: true
+})
+fastify.register(helmet)
+fastify.register(rateLimit, {
+    max: 100, // max requests per time window
+    timeWindow: 1000, // reset window
+})
+fastify.register(fastifyCors, {
+    // add wildcard "*" to origin array if you want any website to get access on your api
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+});
+fastify.register(userRoute)
+fastify.listen({ port: 3000 }, async (err, address) => {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+    // Server is now listening on ${address}
 })
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,44 +1,47 @@
-import express from 'express';
 import { PrismaClient } from "@prisma/client";
-const Router = express.Router();
 const prisma = new PrismaClient();
-Router.get('/users', async function (req, res) {
-    try {
-        const allUsers = await prisma.users.findMany()
-        res.json(allUsers);
-    } catch (e) {
-        console.log(e);
-        return res.status(500).send({ msg: `Error Please try again` });
-    }
-})
 
-Router.post('/users', async function (req, res) {
-    try {
-        console.log(req.body, req.query);
-        if (req.body.first_name != "" && req.body.last_name != "") {
-            await prisma.users.create({
-                data: {
-                    first_name: req.body.first_name,
-                    last_name: req.body.last_name
-                }
-            })
-            return res.status(201).send({ msg: "Success user have been created!" });
-        } else {
-            let errMsg = {}
-            if (req.body.first_name == "") {
-                errMsg['first_name'] = "First name is required field.";
-            }
-            if (req.body.last_name == "") {
-                errMsg['last_name'] = "Last name is required field.";
-            }
-            return res.status(406).send({
-                msg: errMsg,
-            });
+export default async function userRoute(fastify) {
+    fastify.get('/users', async function (req, reply) {
+        try {
+            const allUsers = await prisma.users.findMany()
+            return allUsers;
+        } catch (e) {
+            console.log(e);
+            return reply.status(500).send({ msg: `Error Please try again` });
         }
-    } catch (e) {
-        console.log(e);
-        return res.status(500).send({ msg: `Error Please try again` });
-    }
-})
+    })
 
-export default Router;
+    fastify.post('/users', async (req, reply) => {
+        console.log(req.body);
+        try {
+            if (req.body.first_name != "" && req.body.last_name != "") {
+                await prisma.users.create({
+                    data: {
+                        first_name: req.body.first_name,
+                        last_name: req.body.last_name
+                    }
+                })
+                return reply.status(201).send({ msg: "Success user have been created!" });
+            } else {
+                let errMsg = {}
+                if (req.body.first_name == "") {
+                    errMsg['first_name'] = "First name is required field.";
+                }
+                if (req.body.last_name == "") {
+                    errMsg['last_name'] = "Last name is required field.";
+                }
+                return reply.status(406).send({
+                    msg: errMsg,
+                });
+            }
+        } catch (e) {
+            // throw e;
+            console.log(e);
+            return reply.status(500).send({ msg: `Error Please try again ${e}` });
+        }
+    })
+    fastify.delete('/user/:id', async function () {
+
+    })
+}
